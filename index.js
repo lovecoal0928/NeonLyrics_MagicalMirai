a6d76c76b12bdd85c61b92621ae5f19f4a4a3440
lovecoal928@gmail.com <lovecoal928@gmail.com>
2022-07-06 17:55:02 +0900

    edited-to-new-code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { Player } = TextAliveApp;
 
 // 単語が発声されていたら #text に表示する
-// Show words being vocalized in #text
 const animateWord = function (now, unit) {
   if (unit.contains(now)) {
     document.querySelector("#text").textContent = unit.text;
@@ -9,177 +8,163 @@ const animateWord = function (now, unit) {
 };
 
 // TextAlive Player を作る
-// Instantiate a TextAlive Player instance
 const player = new Player({
   app: {
-    token: "0NLkc52Saxi8vT5f",
-  },
+    token: "2huPtS6L4YdsaZsm", parameters: [
+      {title: "Gradation start color", name: "gradationStartColor", className: "Color", initialValue: "#63d0e2" },
+      {title: "Gradation end color", name: "gradationEndColor", className: "Color", initialValue: "#ff9438" },
+  ]},
+  
   mediaElement: document.querySelector("#media"),
 });
 
-// TextAlive Player のイベントリスナを登録する
-// Register event listeners
-player.addListener({
-  onAppReady,
-  onVideoReady,
-  onTimerReady,
-  onThrottledTimeUpdate,
-  onPlay,
-  onPause,
-  onStop,
-});
+const startVideo = document.querySelector(".open");
+const mediaSlider = document.querySelector('#mediaSlider');
+const positionEl = document.querySelector("#position");
 
-const playBtns = document.querySelectorAll(".open");
-const jumpBtn = document.querySelector("#jump");
-const pauseBtn = document.querySelector("#pause");
-const rewindBtn = document.querySelector("#rewind");
-const positionEl = document.querySelector("#position strong");
-
-const artistSpan = document.querySelector("#artist span");
-const songSpan = document.querySelector("#song span");
-
-/**
- * TextAlive App が初期化されたときに呼ばれる
- *
- * @param {IPlayerApp} app - https://developer.textalive.jp/packages/textalive-app-api/interfaces/iplayerapp.html
- */
-function onAppReady(app) {
-  // TextAlive ホストと接続されていなければ再生コントロールを表示する
-  // Show control if this app is launched standalone (not connected to a TextAlive host)
-  if (!app.managed) {
-    document.querySelector("#control").style.display = "block";
-
-    // 再生ボタン / Start music playback
-    playBtns.forEach((playBtn) =>
-      playBtn.addEventListener("click", () => {
-        player.video && player.requestPlay();
-      })
-    );
-
-    // 歌詞頭出しボタン / Seek to the first character in lyrics text
-    jumpBtn.addEventListener(
-      "click",
-      () =>
-        player.video &&
-        player.requestMediaSeek(player.video.firstChar.startTime)
-    );
-
-    // 一時停止ボタン / Pause music playback
-    pauseBtn.addEventListener(
-      "click",
-      () => player.video && player.requestPause()
-    );
-
-    // 巻き戻しボタン / Rewind music playback
-    rewindBtn.addEventListener(
-      "click",
-      () => player.video && player.requestMediaSeek(0)
-    );
-
-    document
-      .querySelector("#header a")
-      .setAttribute(
-        "href",
-        "https://developer.textalive.jp/app/run/?ta_app_url=https%3A%2F%2Ftextalivejp.github.io%2Ftextalive-app-basic%2F&ta_song_url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DygY2qObZv24"
-      );
-  } else {
-    document
-      .querySelector("#header a")
-      .setAttribute(
-        "href",
-        "https://textalivejp.github.io/textalive-app-basic/"
-      );
-  }
-
-  // 楽曲URLが指定されていなければ マジカルミライ 2022テーマ曲を読み込む
-  // Load a song when a song URL is not specified
-  if (!app.songUrl) {
-    
-    player.createFromSongUrl("https://www.youtube.com/watch?v=ZOTJgXBkJpc");
-  }
-}
-
-/**
- * 動画オブジェクトの準備が整ったとき（楽曲に関する情報を読み込み終わったとき）に呼ばれる
- *
- * @param {IVideo} v - https://developer.textalive.jp/packages/textalive-app-api/interfaces/ivideo.html
- */
-function onVideoReady(v) {
-  // メタデータを表示する
-  // Show meta data
-  artistSpan.textContent = player.data.song.artist.name;
-  songSpan.textContent = player.data.song.name;
-
-  // 定期的に呼ばれる各単語の "animate" 関数をセットする
-  // Set "animate" function
-  let w = player.video.firstWord;
-  while (w) {
-    w.animate = animateWord;
-    w = w.next;
-  }
-}
-
-/**
- * 音源の再生準備が完了した時に呼ばれる
- *
- * @param {Timer} t - https://developer.textalive.jp/packages/textalive-app-api/interfaces/timer.html
- */
-function onTimerReady(t) {
-  // ボタンを有効化する
-  // Enable buttons
-  if (!player.app.managed) {
-    document
-      .querySelectorAll("button")
-      .forEach((btn) => (btn.disabled = false));
-  }
-
-  // 歌詞がなければ歌詞頭出しボタンを無効にする
-  // Disable jump button if no lyrics is available
-  jumpBtn.disabled = !player.video.firstChar;
-}
-/**
- * 動画の再生位置が変更されたときに呼ばれる（あまりに頻繁な発火を防ぐため一定間隔に間引かれる）
- *
- * @param {number} position - https://developer.textalive.jp/packages/textalive-app-api/interfaces/playereventlistener.html#onthrottledtimeupdate
- */
-function onThrottledTimeUpdate(position) {
-  // 再生位置を表示する
-  // Update current position
-  positionEl.textContent = String(Math.floor(position));
-
-  // さらに精確な情報が必要な場合は `player.timer.position` でいつでも取得できます
-  // More precise timing information can be retrieved by `player.timer.position` at any time
-}
-// 再生が始まったら #overlay を非表示に
-// Hide #overlay when music playback started
-function onPlay() {
+startVideo.addEventListener('click', () => {
+  // 再生が始まったら #overlay を非表示に
   setTimeout(function(){
     document.querySelector(".top").style.display = "none";
-    document.querySelector(".main").style.display = "flex";
+    document.querySelector(".main").style.display = "block";
   },600);
-}
+});
+  
+let cursor = document.getElementById('cursor');
+document.addEventListener('mousemove',function(e) {
+  let x = e.clientX;
+  let y = e.clientY;
+  cursor.style.left = x + "px";
+  cursor.style.top = y + "px";
+});
+
+let cursor2 = document.getElementById('cursor-2');
+document.addEventListener('mousemove',function(e) {
+  let x = e.clientX;
+  let y = e.clientY;
+  cursor2.style.left = x + "px";
+  cursor2.style.top = y + "px";
+});
+
+// TextAlive Player のイベントリスナを登録する
+player.addListener({
+  onAppReady(app) {
+  // TextAlive ホストと接続されていなければ再生コントロールを表示する
+    if (!app.managed) {
+      document.querySelector("#control").style.display = "block";
+    }
+    if (!app.songUrl) {
+      document.querySelector("#media").className = "disabled";
+      
+      player.createFromSongUrl("https://piapro.jp/t/RoPB/20220122172830", {
+        video: {
+          // 音楽地図訂正履歴: https://songle.jp/songs/2243651/history
+          beatId: 4086301,
+          chordId: 2221797,
+          repetitiveSegmentId: 2247682,
+          // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FRoPB%2F20220122172830
+          lyricId: 53718,
+          lyricDiffId: 7076
+        },
+      });
+    }
+  },
+  
+  /* パラメタが更新されたら呼ばれる */
+  onAppParameterUpdate: () => {
+    const params = player.app.options.parameters;
+    const sc = player.app.parameters.gradationStartColor, scString = sc ? `rgb(${sc.r}, ${sc.g}, ${sc.b})` : params[0].initialValue;
+    const ec = player.app.parameters.gradationEndColor, ecString = ec ? `rgb(${ec.r}, ${ec.g}, ${ec.b})` : params[1].initialValue;
+    document.body.style.backgroundColor = ecString;
+    document.body.style.backgroundImage = `linear-gradient(0deg, ${ecString} 0%, ${scString} 100%)`;
+  },
+
+  onVideoReady(video) {
+  // 楽曲情報を表示する
+    document.querySelector("#artist").textContent = player.data.song.artist.name;
+    document.querySelector("#song").textContent = player.data.song.name;
+
+  // 定期的に呼ばれる各単語の "animate" 関数をセットする
+    let w = player.video.firstWord;
+    while (w) {
+      w.animate = animateWord;
+      w = w.next;
+    }
+  },
+
+  onTimerReady() {
+  // ボタンを有効化する
+    if (!player.app.managed) {
+      document.querySelector("#control > button").className = "";
+      document.querySelector("#control > input").className = "";
+    }
+  },
+
+  onThrottledTimeUpdate(position) {
+    // 再生位置を表示する
+    positionEl.textContent = String(Math.floor(position));
+    // さらに精確な情報が必要な場合は `player.timer.position` でいつでも取得できます
+  },
+
+  /* 楽曲の再生が始まったら呼ばれる */
+  onPlay() {
+    let element = document.querySelector("#play > i");
+    element.classList.replace('fa-circle-play', 'fa-circle-pause');
+  },
+
+  /* 楽曲の再生が止まったら呼ばれる */
+  onPause() {
+    const element = document.querySelector("#play > i");
+    element.classList.replace('fa-circle-pause', 'fa-circle-play');
+  },
+});
+// 最初の再生ボタン
+// setTimeout(function(){
+//   startVideo.addEventListener(
+//     "click", () => player.video && player.requestPlay()
+//   );
+// },600);
+// 再生ボタン
+document.querySelector("#play > i").addEventListener(
+  "click", (e) => {
+    e.preventDefault();
+    if (player) {
+      if (player.isPlaying) {
+        player.requestPause();
+      } else {
+        player.requestPlay();
+      }
+    }
+    return false;
+});
+
+// メディアシークバー
+mediaSlider.addEventListener(
+  'change', () => player.video && player.requestMediaSeek(mediaSlider.value)
+);
+// 巻き戻しボタン
+document.querySelector("#rewind > i").addEventListener(
+  "click", (e) => {
+    e.preventDefault();
+    player.requestMediaSeek(0);
+});
+// ミュートボタン
+document.querySelector("#mute > i").addEventListener(
+  "click", (e) => {
+    e.preventDefault();
+    player.volueme = 0;
+});
+
+// ボリューム変更
+let music_volume = document.getElementById("volume");
+console.log(music_volume.value);
+player.volume = music_volume.value;
+document.getElementById('vol_range').innerHTML = music_volume.value
+
 // 再生が一時停止・停止したら歌詞表示をリセット
-// Reset lyrics text field when music playback is paused or stopped
 function onPause() {
   document.querySelector("#text").textContent = "";
 }
 function onStop() {
   document.querySelector("#text").textContent = "";
 }
-var cursor = document.getElementById('cursor');
-
-document.addEventListener('mousemove',function(e) {
-  var x = e.clientX;
-  var y = e.clientY;
-  cursor.style.left = x + "px";
-  cursor.style.top = y + "px";
-});
-
-var cursor2 = document.getElementById('cursor-2');
-
-document.addEventListener('mousemove',function(e) {
-  var x = e.clientX;
-  var y = e.clientY;
-  cursor2.style.left = x + "px";
-  cursor2.style.top = y + "px";
-});
